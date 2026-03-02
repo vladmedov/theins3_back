5338eed074a39c3b702580f9709b124a0dff1124
Vladislav Medov <vlad.medov@gmail.com>
2026-03-02 22:43:08 +0100

    fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('STORAGE_URL'),
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
