ab1fd4f8377f383d804a000076d7f42275420b11
Vladislav Medov <vlad.medov@gmail.com>
2026-03-17 13:57:12 +0100

    small design fix

--- app/Nova/_Posts/Post.php
+++ app/Nova/_Posts/Post.php
@@ -408,9 +408,7 @@ abstract class Post extends Resource
                     InsertionCode::make(__('insertion_code.label'), 'show_insertion_code')
                         ->forLayout('related')
                         ->default(false),
-                    Text::make(__('Related posts title'), 'related_title', function ($value) {
-                            return $value ?? __('Publications on this topic');
-                        })
+                    Text::make(__('Related posts title'), 'related_title')
                         ->fullWidth()
                         ->stacked(),
 
