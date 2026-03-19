472590b77be423fbe7b908e878b30dfc4a8ea3b3
Vladislav Medov <vlad.medov@gmail.com>
2026-03-19 22:58:53 +0100

    fix

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -95,7 +95,7 @@ class Author extends Model {
     }
 
     public function getFullNameAttribute() {
-        return $this->first_name . ' ' . $this->last_name;
+        return trim($this->first_name . ' ' . ($this->last_name ?? ''));
     }
 
     public function getAvatarUrlAttribute()
