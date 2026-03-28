b90abf97e20ba62cae102d51061f20e463f50e72
Vladislav Medov <vlad.medov@gmail.com>
2026-03-28 01:06:55 +0100

    fix

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -137,14 +137,11 @@ class FeedController extends Controller
 
     private function feedPath(string $lang, string $suffix): string
     {
-        $lang = $lang ?: 'ru';
         $defaultLang = substr((string) config('app.locale'), 0, 2);
         $lang = $lang ?: $defaultLang;
 
-        if ($lang === $defaultLang) {
-            return '/feed' . $suffix;
-        }
-
-        return "/{$lang}/feed" . $suffix;
+        return $lang === $defaultLang
+            ? '/feed' . $suffix
+            : "/{$lang}/feed" . $suffix;
     }
 }
