59a28d43c0239b5f890a101b01279f0173b91fef
Vladislav Medov <vlad.medov@gmail.com>
2026-04-01 03:02:26 +0200

    fix

--- nova-components/ImageGallery/routes/api.php
+++ nova-components/ImageGallery/routes/api.php
@@ -10,11 +10,13 @@ Route::post('/upload-image', function (Request $request) {
 
     $imageId = uniqid('', true);
     $file = $request->file('file');
+    $languageCode = app()->getLocale();
+    $disk = ImageService::publicDiskForLanguage($languageCode);
 
     $targetDir = ImageService::getImagePath($imageId, ImageService::TYPE_CONTENT_IMAGE, ImageService::SIZE_ORIGINAL);
-    $path = $file->store($targetDir, 'public');
+    $path = $file->store($targetDir, $disk);
 
-    ImageService::createImageVariants($imageId, $path, ImageService::TYPE_CONTENT_IMAGE);
+    ImageService::createImageVariants($imageId, $path, ImageService::TYPE_CONTENT_IMAGE, $languageCode);
 
     return response()->json([
         'id' => $imageId,
