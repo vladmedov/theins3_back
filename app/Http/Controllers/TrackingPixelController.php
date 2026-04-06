647eca2c875ad3b6dfebc130615785e014062e1e
Vladislav Medov <vlad.medov@gmail.com>
2026-04-06 23:44:38 +0200

    view without IP

--- app/Http/Controllers/TrackingPixelController.php
+++ app/Http/Controllers/TrackingPixelController.php
@@ -11,32 +11,22 @@ class TrackingPixelController extends Controller
     public function trackView($language_code, Request $request)
     {
         $postSlug = $request->input('post_slug');
-        $ip = $request->input('ip');
-        
-        if (!$postSlug || !$ip) {
+
+        if (!$postSlug) {
             return response()->json(['status' => 'ERROR_INVALID_REQUEST'], 400);
         }
-        
+
         $post = Post::select('id')
             ->where('slug', $postSlug)
             ->where('language_code', $language_code)
             ->where('status', Post::STATUS_PUBLISHED)
             ->first();
-        
+
         if (!$post) {
             return response()->json(['status' => 'ERROR_POST_NOT_FOUND'], 404);
         }
-        
+
         $redis = Redis::connection()->client();
-        
-        $ipKey = "view_ip:{$post->id}:{$ip}";
-        
-        if ($redis->exists($ipKey)) {
-            return response()->json(['status' => 'ERROR_IP_LIMIT']);
-        }
-        
-        $redis->setex($ipKey, 3600, '1');
-        
         $redis->incr("view_count:{$post->id}");
         
         return response()->json(['status' => 'SUCCESS']);
