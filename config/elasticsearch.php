079eabe1809b38fd375927f1d757ec12efc8b90e
Vladislav Medov <vlad.medov@gmail.com>
2026-04-15 14:20:08 +0200

    ngram search added

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -5,6 +5,34 @@ return [
      | Custom index mappings for language-aware inflection search.
      */
     'indices' => [
+        'settings' => [
+            'default' => [
+                'analysis' => [
+                    'filter' => [
+                        'ti_ngram_filter' => [
+                            'type' => 'ngram',
+                            'min_gram' => 3,
+                            'max_gram' => 12,
+                        ],
+                    ],
+                    'analyzer' => [
+                        'ti_ngram_index' => [
+                            'tokenizer' => 'standard',
+                            'filter' => [
+                                'lowercase',
+                                'ti_ngram_filter',
+                            ],
+                        ],
+                        'ti_ngram_search' => [
+                            'tokenizer' => 'standard',
+                            'filter' => [
+                                'lowercase',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
         'mappings' => [
             'default' => [
                 'properties' => [
@@ -24,6 +52,16 @@ return [
                                 'analyzer' => 'english',
                                 'search_analyzer' => 'english',
                             ],
+                            'ru_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
+                            'en_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
                         ],
                     ],
                     'lead' => [
@@ -38,6 +76,16 @@ return [
                                 'type' => 'text',
                                 'analyzer' => 'english',
                                 'search_analyzer' => 'english',
+                            ],
+                            'ru_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
+                            'en_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
                             ],
                         ],
                     ],
@@ -54,6 +102,16 @@ return [
                                 'analyzer' => 'english',
                                 'search_analyzer' => 'english',
                             ],
+                            'ru_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
+                            'en_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
                         ],
                     ],
                     'authors' => [
@@ -68,6 +126,16 @@ return [
                                 'type' => 'text',
                                 'analyzer' => 'english',
                                 'search_analyzer' => 'english',
+                            ],
+                            'ru_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
+                            'en_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
                             ],
                         ],
                     ],
@@ -84,6 +152,16 @@ return [
                                 'analyzer' => 'english',
                                 'search_analyzer' => 'english',
                             ],
+                            'ru_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
+                            'en_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
                         ],
                     ],
                     'tags' => [
@@ -98,6 +176,16 @@ return [
                                 'type' => 'text',
                                 'analyzer' => 'english',
                                 'search_analyzer' => 'english',
+                            ],
+                            'ru_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
+                            ],
+                            'en_ngram' => [
+                                'type' => 'text',
+                                'analyzer' => 'ti_ngram_index',
+                                'search_analyzer' => 'ti_ngram_search',
                             ],
                         ],
                     ],
